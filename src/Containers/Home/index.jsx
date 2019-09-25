3b49859119daa166e27a38c9a8f86303d4092825
Donna Stefania Adamo <donnaa@lagash.com>
2019-09-25 12:51:01 -0300

    sidebar and config

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Container, Col, Row } from 'react-bootstrap/'
+import {Sidebar} from '../../Components'
 
 const Home = () => {
     return (
         <Container>
             <Row>
                 <Col md={4}>
-                
+                    <Sidebar />
                 </Col>
                 <Col md={4}>
                 

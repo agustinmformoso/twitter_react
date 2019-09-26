93d16fc47bdf250ac58457c4c808ee7f38f4501a
Donna Stefania Adamo <donnaa@lagash.com>
2019-09-26 14:25:03 -0300

    icons fixed

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import Button from '../Button';
 import { GLOBAL } from '../../Config/global';
-import { TwitterIcon, BellIcon, BookmarkIcon, EmailIcon, HashtagIcon, ListIcon, HomeIcon, DotsIcon, ProfilePic } from '../../Resources/Images/index';
+import { ProfilePic } from '../../Resources/Images/index';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="sidebar__box">
-            <Button className="sidebar__box__button" description="" icon={<img src={TwitterIcon} className="button_icon"/>}/>
-            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_HOME} icon={<img src={HomeIcon} className="button_icon"/>}/>
-            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_EXPLORE} icon={<img src={HashtagIcon} className="button_icon"/>}/>
-            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_NOTIF} icon={<img src={BellIcon} className="button_icon"/>}/>
-            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_DM} icon={<img src={EmailIcon} className="button_icon"/>}/>
-            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_SAVED} icon={<img src={BookmarkIcon} className="button_icon"/>}/>
-            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_LISTS} icon={<img src={ListIcon} className="button_icon"/>}/>
-            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_PROFILE} icon={<img src={ProfilePic} className="button_icon"/>}/>
-            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_OPTIONS} icon={<img src={DotsIcon} className="button_icon"/>}/>
+            <Button className="sidebar__box__button" description="" icon={<i class="fab fa-twitter sidebar__box__button__icon sidebar__box__button__icon--twitter"></i>}/>
+            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_HOME} icon={<i class="fas fa-home sidebar__box__button__icon"></i>}/>
+            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_EXPLORE} icon={<i class="fas fa-hashtag sidebar__box__button__icon"></i>}/>
+            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_NOTIF} icon={<i class="far fa-bell sidebar__box__button__icon"></i>}/>
+            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_DM} icon={<i class="far fa-envelope sidebar__box__button__icon"></i>}/>
+            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_SAVED} icon={<i class="far fa-bookmark sidebar__box__button__icon"></i>}/>
+            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_LISTS} icon={<i class="far fa-list-alt sidebar__box__button__icon"></i>}/>
+            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_PROFILE} icon={<img src={ProfilePic} className="sidebar__box__button__icon sidebar__box__button__icon--profile"/>}/>
+            <Button className="sidebar__box__button" description={GLOBAL.SIDEBAR.BUTTON_OPTIONS} icon={<i class="fas fa-ellipsis-h sidebar__box__button__icon"></i>}/>
             <Button className="sidebar__box__button sidebar__box__button--twittear" description="Twittear"/>
             </div>
         </div>
